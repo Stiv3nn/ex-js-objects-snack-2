38200f406efa744ea473313e42e2f97ebefddcf0
Stiven hoxha <stivenhoxha007@gmail.com>
2025-04-23 11:32:27 +0200

    code question

---
+++ script.js
@@ -0,0 +1,82 @@
+// 🏆 Code Question 1
+// const hamburger = { name: "Cheese Burger", weight: 250 };
+// const secondBurger = hamburger;
+// secondBurger.name = 'Double Cheese Burger';
+// secondBurger.weight = 500;
+// ​
+// console.log(hamburger.name); // Double Cheese Burger
+// console.log(secondBurger.name ); // Double Cheese Burger
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 1
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+// 🏆 Code Question 2
+// const hamburger = { 
+//     name: "Cheese Burger", 
+//     weight: 250,
+//     ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+// };
+// ​
+// const secondBurger = {...hamburger};
+// secondBurger.ingredients[0] = "Salad";
+// ​
+// console.log(hamburger.ingredients[0]); // Salad
+// console.log(secondBurger.ingredients[0]); // Salad
+// P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 1
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+// 🏆 Code Question 3
+// const hamburger = { 
+//     name: "Cheese Burger", 
+//     weight: 250,
+//     maker: {
+//         name: "Anonymous Chef",
+//         restaurant: {
+//             name: "Hyur's Burgers",
+//             address: "Main Street, 123",
+//             isOpen: true,
+//         },
+//         age: 29
+//     }
+// };
+// ​
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 9 perche l'oggetto originale ne contiene 2 oggetti annidaati all'interno. piccolo trabocchetto
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+// 🏆 Code Question 4
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+}
+
+const chefCopia = {...chef};
+
+console.log(chefCopia);
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché? SPREAD OPERATOR, perchè mi copia la funzione in modo corretto
+
+const restaurant = {
+    name: "Hyur's Burgers",
+    address: {
+        street: 'Main Street',
+        number: 123,
+    },
+    openingDate: new Date(2025, 3, 11),
+    isOpen: false,
+};
+
+const restaurantCopy = structuredClone(restaurant);
+
+console.log(restaurantCopy);
+
+// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché? structuredClone(), perchè mi copia gli oggetti particolari, in questo caso l'oggetto data in maniera corretta
